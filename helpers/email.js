49162212db2d12d74a5d4728b68e33c7928cc164
Diego Pardo <diegopar03@gmail.com>
2023-02-07 10:16:38 -0600

    routing fixed

--- helpers/email.js
+++ helpers/email.js
@@ -27,7 +27,7 @@ const emailRegistro = async datos => {
         htmlContent: `
             <p style="display:block; color:#374151; font-weight:bold; text-transform:uppercase;">Hola: ${nombre}, confirma tu cuenta en APV</p>
             <p style="display:block; color:#374151; font-weight:bold; text-transform:uppercase;">Tu cuenta esta casi lista, solo debes comprobarla en el siguiente enlace:</p>
-            <a style="background-color:#4f46e5; font-weight:bold; color:white; padding:10px; border-radius:5px; text-decoration:none; text-transform:uppercase; margin-top:20px; margin-bottom:20px;" href="http://127.0.0.1:5173/confirmar/${token}">Confirmar Cuenta</a>
+            <a style="background-color:#4f46e5; font-weight:bold; color:white; padding:10px; border-radius:5px; text-decoration:none; text-transform:uppercase; margin-top:20px; margin-bottom:20px;" href="http://${process.env.FRONTEND_URL}/${token}">Confirmar Cuenta</a>
             <p style="display:block; color:#374151; font-weight:bold; text-transform:uppercase;">Si tu no creaste esta cuenta, puedes ignorar este mensaje<p>
         `
     })
@@ -62,7 +62,7 @@ const emailRecuperarPassword = async datos => {
         htmlContent: `
             <p style="display:block; color:#374151; font-weight:bold; text-transform:uppercase;">Hola: ${nombre}, has solicitado reestablecer tu contraseña</p>
             <p style="display:block; color:#374151; font-weight:bold; text-transform:uppercase;">sigue el siguiente enlace para generar una nueva:</p>
-            <a style="background-color:#4f46e5; font-weight:bold; color:white; padding:10px; border-radius:5px; text-decoration:none; text-transform:uppercase; margin-top:20px; margin-bottom:20px;" href="http://127.0.0.1:5173/olvide-password/${token}">Reestablecer Contraseña</a>
+            <a style="background-color:#4f46e5; font-weight:bold; color:white; padding:10px; border-radius:5px; text-decoration:none; text-transform:uppercase; margin-top:20px; margin-bottom:20px;" href="http://${process.env.FRONTEND_URL}/olvide-password/${token}">Reestablecer Contraseña</a>
             <p style="display:block; color:#374151; font-weight:bold; text-transform:uppercase;">Si tu no solicitaste este email, puedes ignorar este mensaje<p>
         `
     })
